3a9cc2b14a84fca3134b47e8294b6bdb282e1fbb
megan liken <meganliken@megans-MacBook-Pro-2.local>
2017-02-26 16:21:07 -0600

    added jaws db

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,17 @@
 var mysql = require("mysql");
+var connection;
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "g70224933@IIE",
-  database: "burgers_db"
+if (process.env.JAWSDB_URL){
+	connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+
+	connection = mysql.createConnection({
+  	host: "localhost",
+  	user: "root",
+  	password: "g70224933@IIE",
+  	database: "burgers_db"
 });
+};
 
 
 connection.connect(function(err) {
